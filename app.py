fa529efecdbd40d44700d465c75c873dead30061
VPC solar <125677853+VPC-Solar@users.noreply.github.com>
2026-05-11 16:59:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ with col1:
     st.caption("VPC Solar © 2026")
 
 with col2:
-    if st.button("📞 تواصل معنا"):
+    if st.pills("📞 تواصل معنا"):
 
         st.info("""
         📧 Email: vpcsolar@gmail.com
